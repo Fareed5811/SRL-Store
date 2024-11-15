3cf7114592df4083b584f9a5bc74cb97a1850899
Fareed5811 <faridakiwat1@gmail.com>
2024-11-15 09:31:14 +0530

    simple layout

---
+++ shop.js
@@ -0,0 +1,12 @@
+// JavaScript code for filtering products by category
+function filterProducts(category) {
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(product => {
+        if (category === 'all' || product.dataset.category === category) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
+}
